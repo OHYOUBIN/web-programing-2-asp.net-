7ae9cd3b22b4a48bc7e19c5a1648e8b24d09fada
OHYOUBIN <90302922+OHYOUBIN@users.noreply.github.com>
2022-12-01 11:20:19 +0900

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Loanstatus] (
+    lusername   VARCHAR (50) NOT NULL,
+   Booksname VARCHAR (50) NOT NULL,
+    deadline  VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([lusername] ASC)
+);
